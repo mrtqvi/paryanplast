7e7137b17bd83cc275bfc8ae902b54d6d67b8790
yasin taghavi <mrtqvi@yahoo.com>
2023-08-29 00:39:57 +0330

    complete intro section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: {
+        DEFAULT: '1rem',
+        md: '0',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
     },
     extend: {
       fontFamily : {
