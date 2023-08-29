876b910d922eab567cec71bc2bae450352ce78f7
yasin taghavi <mrtqvi@yahoo.com>
2023-08-29 14:31:03 +0330

    complete product section

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,15 @@
+// script for intro section
+window.addEventListener('load', () => {
+  const introImages = document.querySelectorAll('.intro-image');
+  const introTitle = document.querySelector('.intro-title');
+
+  introImages.forEach(item => item.style.transform = "translate(0)");
+  introTitle.classList.remove('scale-50', 'opacity-0');
+});
+
+
+
+
 let readMore = document.querySelector("#readMore");
 let hiddenDiv = document.querySelector("#hiddenDiv");
 let aboutUsParag = document.querySelector("#aboutUsParag");
@@ -6,3 +18,4 @@ readMore.addEventListener("click", () => {
   hiddenDiv.classList.toggle("hidden");
   aboutUsParag.classList.toggle("h-full");
 });
+
