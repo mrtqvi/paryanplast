af8a705b9858d38ac62b5472968d35a265106db0
Ali Rough <ali.hastam2015@gmail.com>
2023-08-29 11:31:32 +0330

    fix some bug in About Us

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,11 @@
 let readMore = document.querySelector("#readMore");
 let hiddenDiv = document.querySelector("#hiddenDiv");
 let aboutUsParag = document.querySelector("#aboutUsParag");
-
-readMore.addEventListener("click", () => {
+let isAboutUsOpen = false;
+readMore.addEventListener("click", (e) => {
   hiddenDiv.classList.toggle("hidden");
   aboutUsParag.classList.toggle("h-full");
+  console.log(e.target.value);
+  e.target.innerHTML = isAboutUsOpen ? "اطلاعات بیشتر..." : "نمایش کمتر";
+  isAboutUsOpen = !isAboutUsOpen;
 });
