64be4b7cf75eb47bf015e07d6d1c7cccca0b4ae3
Ali Rough <ali.hastam2015@gmail.com>
2023-08-30 09:49:43 +0330

    add services section

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,10 @@
+// const Swiper= "swiper/swiper-element-bundle.min.mjs";
 let readMore = document.querySelector("#readMore");
 let hiddenDiv = document.querySelector("#hiddenDiv");
 let aboutUsParag = document.querySelector("#aboutUsParag");
+
 let isAboutUsOpen = false;
+
 readMore.addEventListener("click", (e) => {
   hiddenDiv.classList.toggle("hidden");
   aboutUsParag.classList.toggle("h-full");
@@ -9,3 +12,19 @@ readMore.addEventListener("click", (e) => {
   e.target.innerHTML = isAboutUsOpen ? "اطلاعات بیشتر..." : "نمایش کمتر";
   isAboutUsOpen = !isAboutUsOpen;
 });
+
+// services
+
+// const swiper = new Swiper("#services-swiper", {
+//   // Optional parameters
+//   // direction: "h",
+//   loop: true,
+//   slidesPerView: 1,
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+
+//   autoplay: true,
+// });
