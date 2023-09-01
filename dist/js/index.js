058c12388dbf8bfcec9589ad1aaf5f1d6a9f4609
Ali Rough <ali.hastam2015@gmail.com>
2023-09-02 00:59:49 +0330

    add video in services

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,20 +1,18 @@
 const header = document.querySelector("#header");
 
-window.addEventListener('scroll', (event) => {
-
+window.addEventListener("scroll", (event) => {
   if (window.scrollY === 0) {
-    header.classList.remove('bg-white');
-  }
-  else {
-    header.classList.add('bg-white');
+    header.classList.remove("bg-white");
+  } else {
+    header.classList.add("bg-white");
   }
 
   const product = document.querySelector("#product");
   let productPositionInfo = product.getBoundingClientRect();
 
   if (productPositionInfo.y < 150) {
-    const products = document.querySelectorAll('.product-container');
-    products.forEach(item => item.style.transform = "translate(0)");
+    const products = document.querySelectorAll(".product-container");
+    products.forEach((item) => (item.style.transform = "translate(0)"));
   }
 });
 
@@ -25,7 +23,6 @@ window.addEventListener("load", () => {
 
   introImages.forEach((item) => (item.style.transform = "translate(0)"));
   introTitle.classList.remove("scale-50", "opacity-0");
-
 });
 
 // script for about us section
@@ -39,11 +36,66 @@ let isAboutUsOpen = false;
 readMore.addEventListener("click", (e) => {
   hiddenDiv.classList.toggle("hidden");
   aboutUsParag.classList.toggle("h-full");
+
   e.target.innerHTML = isAboutUsOpen ? "اطلاعات بیشتر..." : "نمایش کمتر";
   isAboutUsOpen = !isAboutUsOpen;
 });
+// services swiper
 
 // services swiper
+let allServiceImg = document.querySelectorAll(".services-path");
+
+let servicesSwiperElem = document.querySelector("#services-swiper");
+let videos = [
+  { data: "high", src: "about-us-control" },
+  { data: "control", src: "about-us-control" },
+  { data: "packing", src: "about-us-packing" },
+];
+const closeModalHandler = () => {
+  let closeModal = document.querySelector("#services-Modal");
+  closeModal.remove();
+};
+
+allServiceImg.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    let clickOnSvg =
+      e.target.parentElement.parentElement.parentElement.parentElement
+        .parentElement.classList;
+    let showModal = false;
+
+    clickOnSvg.forEach((item) => {
+      item === "swiper-slide-active" && (showModal = true);
+    });
+
+    let video = videos.find((video) => {
+      return video.data == e.target.dataset.parentName;
+    });
+
+    showModal &&
+      servicesSwiperElem.insertAdjacentHTML(
+        "beforebegin",
+        ` <div
+      id="services-Modal"
+        class="z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center"
+      >
+        <div class="relative w-9/12 z-10">
+          <video src="../videos/${video.src}.mp4" autoplay loop></video>
+          <button
+          onclick="closeModalHandler()"
+            class="closeModal absolute top-0 p-4 text-white text-2xl hover:text-red-500"
+          >
+            X
+          </button>
+        </div>
+        <button
+        onclick=${"closeModalHandler()"}
+          class="closeModal absolute h-full w-full bg-black opacity-50"
+        ></button>
+      </div>`
+      );
+  });
+});
+
 const servicesSwiper = new Swiper("#services-swiper", {
   slidesperview: 1,
   loop: true,
@@ -52,7 +104,7 @@ const servicesSwiper = new Swiper("#services-swiper", {
   autoplay: true,
   breakpoints: {
     768: {
-      spaceBetween: 10,
+      spaceBetween: 100,
       centeredSlides: true,
       setWrapperSize: true,
       slideToClickedSlide: true,
